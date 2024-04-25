763fe8239307781a83790c76f1cb4cd40381785c
Just_Ho_it <zalop3128@kakao.com>
2024-04-26 03:16:24 +0900

    변수, 값

---
+++ Value.js
@@ -0,0 +1,27 @@
+//문자열 값 (원시 값)
+var str = 'word'
+str.toUpperCase();
+
+console.log(str);
+
+const un = undefined
+const nu =  null
+const bool = false
+const num = 0
+const str2 = '문자열'
+
+console.log(un);
+console.log(nu);
+console.log(bool);
+console.log(num);
+console.log(str2);
+
+if(num === 0){
+    console.log('num은 0 입니다.');
+}
+/*
+* 동등 연산자를 사용할때
+* 엄겯한 비교 -> ===
+* 느슨한 비교 -> ==
+* */
+
---
+++ Variable.js
@@ -0,0 +1,12 @@
+//선언
+let lang;
+//할당
+lang = 'JS'
+//재할당
+lang = 'JS2'
+//선언과 동시에 할당
+let lang2 = 'JavaScript'
+//복합할당 연산자
+let count = 0;
+count = count + 1; //예상하기 쉬움
+count += 1; // 보기 좋을 수 있으나
