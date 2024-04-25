e834746777d03caafd8e07bb6eb7ecdb9303734a
Just_Ho_it <zalop3128@kakao.com>
2024-04-26 03:22:58 +0900

    함수 선언

---
+++ funtion.js
@@ -0,0 +1,21 @@
+//함수 선언문
+function sum(param1, param2){
+ return param1 + param2;
+}
+
+//메서드
+const obj = {
+    prop : function () {
+
+    }
+}
+
+//생성자 함수
+function MyObj() {
+    //
+}
+
+//화살표 함수
+const arrowFunc = () => {}
+
+sum(1, 2);
